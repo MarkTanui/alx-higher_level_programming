5020e93b5f396a0aba4c92288e85d8511491f4a9
Mark Tanui <marktanui75@gmail.com>
2021-09-07 16:46:26 +0300

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(0, len(str)):
-        print(chr(ord(str[i]) - 32))
+        print(chr(ord(str[i]) - 32), end='')
 
 uppercase(str = "hello its me")
