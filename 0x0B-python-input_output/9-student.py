2491fbeacb3b837f6d90371c9e1b6b4109c6bf5d
Mark Tanui <marktanui75@gmail.com>
2021-10-13 14:36:41 +0300

    bug fixed

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -13,9 +13,13 @@ class Student:
     """
 
     def __init__(self, first_name, last_name, age):
+        """representation of a student.
+        initialization of public variables.
+        """
         first_name = self.first_name
         last_name = self.last_name
         age = self.age
 
     def to_json(self):
+        """returns the class as a dictionary descrpition"""
         return self.__dict__
