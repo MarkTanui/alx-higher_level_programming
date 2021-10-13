ebd89b973dc2a56983173324dc03a500ff519a08
Mark Tanui <marktanui75@gmail.com>
2021-10-13 23:13:49 +0300

    subclass, right?

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" a function that returns True if the object is an instance of a class\
+that inherited (directly or indirectly) from the specified class;\
+otherwise False
+"""
+
+
+def inherits_from(obj, a_class):
+    """checks if an  object is an instance of a class that inherited"""
+    return (issubclass(obj, a_class))
