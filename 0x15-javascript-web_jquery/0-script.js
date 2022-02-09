4dea98d52380f578173904608ce5440bbca6c299
tanui <marktanui75@gmail.com>
2022-02-10 00:05:09 +0300

    Jquery scripts

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,7 +1,2 @@
-#!/usr/bin/node
-/* JavaScript script that updates the text color of the <header> element to red
- * (#FF0000)
- */
-
 const myHeader = document.querySelector('header');
 myHeader.style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function () {
+  const myHeader = document.querySelector('header');
+  myHeader.style.color = '#FF0000';
+});
